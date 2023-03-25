a59bd0cc54b209de075094db32ee18417104a3cb
Sara Lieto <saralieto@saras-mbp-2.myfiosgateway.com>
2023-03-24 20:06:31 -0400

    [001] Basic Xcode project

---
+++ wfh-barista/wfh_baristaApp.swift
@@ -0,0 +1,17 @@
+//
+//  wfh_baristaApp.swift
+//  wfh-barista
+//
+//  Created by Sara Lieto on 3/24/23.
+//
+
+import SwiftUI
+
+@main
+struct wfh_baristaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
